d5b49e3c36c44a1f3e2b21543151d61255345990
chrisqiuu <chriscanedaquesada@gmail.com>
2022-11-27 17:43:46 +0800

    Add quote and about me section

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // // console.log('Hello, World');
 
+// hamburger collapse-expand
 const menuBtn = document.querySelector('.hamburger');
 const mediumNav = document.querySelector('.navbar-md ul');
 
 menuBtn.addEventListener('click', function() {
 	menuBtn.classList.toggle('is-active');
 	mediumNav.classList.toggle('is-active');
+});
+
+// header reveal background
+const navScroll = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY >= 60) {
+		navScroll.classList.add('navbar-scroll-at-60');
+	} else if (window.scrollY < 60) {
+		navScroll.classList.remove('navbar-scroll-at-60');
+	}
 });
